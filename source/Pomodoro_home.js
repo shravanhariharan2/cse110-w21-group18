c790982ef17db283630975bdb21af4d96b3dc95d
sbenetz <60725970+sbenetz@users.noreply.github.com>
2021-02-10 10:50:33 -0800

    Disable start button after started

--- source/Pomodoro_home.js
+++ source/Pomodoro_home.js
@@ -1,10 +1,10 @@
 // times that we will want to count down from
 
-const POMO_TIME = 25;
+const pomoTime = 25;
 
-const SHORT_BREAK = 5;
+const shortBreak = 5;
 
-const LONG_BREAK = 15;
+const longBreak = 15;
 
 //displays time on html
 function timeSet(minutes, seconds){
@@ -36,10 +36,11 @@ function startTimer(length){
         return;
     }
     }, 1000);
-  
+    startButton.disabled = false;
 }
 //this is what we will call for starting a pomo
 var startButton = document.getElementById("start");
 startButton.addEventListener("click", function(){
-    startTimer(POMO_TIME);
+    startTimer(pomoTime);
+    startButton.disabled = true;
 })
