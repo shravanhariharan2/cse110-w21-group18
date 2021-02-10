6f8232535db13cdfbe684bc7795dc24b7f814883
sbenetz <60725970+sbenetz@users.noreply.github.com>
2021-02-10 10:35:22 -0800

    Update Pomodoro_home.js

--- source/Pomodoro_home.js
+++ source/Pomodoro_home.js
@@ -1,10 +1,10 @@
 // times that we will want to count down from
 
-const pomoTime = 25;
+const POMO_TIME = 25;
 
-const shortBreak = 5;
+const SHORT_BREAK = 5;
 
-const longBreak = 15;
+const LONG_BREAK = 15;
 
 //displays time on html
 function timeSet(minutes, seconds){
@@ -41,5 +41,5 @@ function startTimer(length){
 //this is what we will call for starting a pomo
 var startButton = document.getElementById("start");
 startButton.addEventListener("click", function(){
-    startTimer(pomoTime);
+    startTimer(POMO_TIME);
 })
