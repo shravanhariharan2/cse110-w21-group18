f88a05b2f1515cbbe1915eee17ff96d2b3cf047d
trinpham <ttpham@ucsd.edu>
2021-02-11 19:10:56 -0800

    add short/long breaks

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -6,6 +6,16 @@ const SHORT_BREAK = 5;
 
 const LONG_BREAK = 15;
 
+const NUM_OF_SESSIONS_IN_POMO = 4;
+
+let sessionNumber = 0;
+let isBreak = false;
+
+//listens to start initial pomo session
+let startButton = document.getElementById("start");
+startButton.addEventListener("click", startSession);
+
+
 //displays time on html
 function setTime(minutes, seconds){
     //two digits nums for mins and secs
@@ -19,9 +29,47 @@ function startTimer(durationInMinutes){
     let minutes = durationInMinutes-1;
     let seconds = 59;
     let finished = false;
+    let endedEarly = false;
+
     //runs every second
     let timer = setInterval(function() {
+
         setTime(minutes, seconds);
+
+        //listens to end the timer
+        let eventListenerAdded = false;
+        if (!eventListenerAdded) {
+            startButton.addEventListener("click", endSession);
+            eventListenerAdded = true;
+        }
+        function endSession() {
+            finished = true;
+            eventListenerAdded = false;
+            startButton.disabled = false;
+            endedEarly = true;
+            startButton.removeEventListener("click", endSession);
+        }
+
+        //end timer
+        if (finished == true){
+            clearInterval(timer);
+            //go to next session
+            if (!endedEarly) {
+                //update session indicators
+                if (isBreak) {
+                    isBreak = false;
+                }
+                else {
+                    sessionNumber++;
+                    isBreak = true;
+                }
+            }
+            console.log(sessionNumber);
+            updateUIForSessionEnd();
+            document.getElementById("start").value = "Start";
+            startButton.addEventListener("click", startSession);
+            return;
+        }
      
         if (seconds <= 0) {
             minutes --;
@@ -31,16 +79,54 @@ function startTimer(durationInMinutes){
             }
         }
         seconds--;
-        if (finished == true){
-            clearInterval(timer);
-            return;
-        }
+
     }, 1000);
     startButton.disabled = false;
 }
-//this is what we will call for starting a pomo
-let startButton = document.getElementById("start");
-startButton.addEventListener("click", function(){
-    startTimer(POMO_TIME);
-    startButton.disabled = true;
-});
+
+//this is what we will call for starting a timer session
+function startSession() {
+    startButton.removeEventListener("click", startSession);
+    document.getElementById("start").value = "End";
+    startButton.disabled = false;
+    //start pomo session
+    if (!isBreak) {
+        startTimer(POMO_TIME);
+    }
+    else {
+        //start short break
+        if (sessionNumber != NUM_OF_SESSIONS_IN_POMO) {
+            startTimer(SHORT_BREAK);
+        }
+        //start long break
+        else {
+            startTimer(LONG_BREAK);
+            sessionNumber = 0;
+        }
+    }
+};
+
+//this is what we will call for updating UI elements
+function updateUIForSessionEnd() {
+    document.getElementById("pomo").style.textDecoration = "none";
+    document.getElementById("short-break").style.textDecoration = "none";
+    document.getElementById("long-break").style.textDecoration = "none";
+    
+    //is pomo session
+    if (!isBreak) {
+        document.getElementById("pomo").style.textDecoration = "underline";
+        setTime(POMO_TIME, 0);
+    }
+    else {
+        //is short break
+        if (sessionNumber != NUM_OF_SESSIONS_IN_POMO) {
+            document.getElementById("short-break").style.textDecoration = "underline";
+            setTime(SHORT_BREAK, 0);
+        }
+        //is long break
+        else {
+            document.getElementById("long-break").style.textDecoration = "underline";
+            setTime(LONG_BREAK, 0);
+        }
+    }
+}
