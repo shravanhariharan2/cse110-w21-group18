8b3c4d04ba1dadb80ce5c3e5962ef7dacd7e57e8
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-13 20:09:07 -0800

    Lint JS w/ linter

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -10,134 +10,123 @@ const NUM_OF_SESSIONS_IN_POMO = 4;
 
 const DEFAULT_LOCALE = 'en-US';
 
-const TIMER_DISPLAY_SETTINGS = {minimumIntegerDigits: 2, useGrouping:false};
+const TIMER_DISPLAY_SETTINGS = { minimumIntegerDigits: 2, useGrouping: false };
 
 let sessionNumber = 0;
 let isBreak = false;
 let timer;
 
-//listens to start initial pomo session
-let startButton = document.getElementById("start");
-startButton.addEventListener("click", startTimerSession);
+// listens to start initial pomo session
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', startTimerSession);
 
-
-function setTimeOnUI(minutes, seconds){
-    //two digits nums for mins and secs
-    document.getElementById("time").innerHTML = minutes.toLocaleString(DEFAULT_LOCALE, TIMER_DISPLAY_SETTINGS)
-         + " : " + seconds.toLocaleString(DEFAULT_LOCALE, TIMER_DISPLAY_SETTINGS);
+function setTimeOnUI(minutes, seconds) {
+  // two digits nums for mins and secs
+  document.getElementById('time').innerHTML = `${minutes.toLocaleString(DEFAULT_LOCALE, TIMER_DISPLAY_SETTINGS)
+  } : ${seconds.toLocaleString(DEFAULT_LOCALE, TIMER_DISPLAY_SETTINGS)}`;
 }
 
 // function to actually run the timer with input length
-function startTimer(durationInMinutes){
-    //starting time
-    let minutes = durationInMinutes-1;
-    let seconds = 59;
-    let isfinished = false;
+function startTimer(durationInMinutes) {
+  // starting time
+  let minutes = durationInMinutes - 1;
+  let seconds = 59;
+  let isfinished = false;
 
-    //runs every second
-    timer = setInterval(function() {
+  // runs every second
+  timer = setInterval(() => {
+    setTimeOnUI(minutes, seconds);
 
-        setTimeOnUI(minutes, seconds);
+    // end timer
+    if (isfinished === true) {
+      clearInterval(timer);
 
-        //end timer
-        if (isfinished === true){
-            clearInterval(timer);
+      // update session indicators
+      if (isBreak) {
+        isBreak = false;
+        startButton.removeEventListener('click', endTimerSession);
+        startButton.addEventListener('click', startTimerSession);
 
-            //update session indicators
-            if (isBreak) {
-                isBreak = false;
-                startButton.removeEventListener("click", endTimerSession);
-                startButton.addEventListener("click", startTimerSession);
+        updateUIForSessionEnd();
+      } else {
+        sessionNumber += 1;
+        isBreak = true;
 
-                updateUIForSessionEnd();
-            }
-            else {
-                sessionNumber++;
-                isBreak = true;
+        updateUIForSessionEnd();
+        // will also update UI button
+        startTimerSession();
+      }
+      return;
+    }
 
-                updateUIForSessionEnd();
-                //will also update UI button
-                startTimerSession();
-            }
-            return;
-        }
-     
-        if (seconds <= 0) {
-            minutes --;
-            seconds = 60;
-            if (minutes < 0) {
-                isfinished = true;
-            }
-        }
-        seconds--;
-
-    }, 1000);
-    startButton.disabled = false;
+    if (seconds <= 0) {
+      minutes -= 1;
+      seconds = 60;
+      if (minutes < 0) {
+        isfinished = true;
+      }
+    }
+    seconds -= 1;
+  }, 1000);
+  startButton.disabled = false;
 }
 
 function startTimerSession() {
-    //update button text for session start
-    startButton.value = "End";
+  // update button text for session start
+  startButton.value = 'End';
 
-    startButton.removeEventListener("click", startTimerSession);
-    startButton.addEventListener("click", endTimerSession);
+  startButton.removeEventListener('click', startTimerSession);
+  startButton.addEventListener('click', endTimerSession);
 
-    //start pomo session
-    if (!isBreak) {
-        startTimer(POMO_TIME);
+  if (!isBreak) {
+    startTimer(POMO_TIME);
+  } else {
+    const isShortBreak = sessionNumber !== NUM_OF_SESSIONS_IN_POMO;
+    if (isShortBreak) {
+      startTimer(SHORT_BREAK);
+    } else {
+      startTimer(LONG_BREAK);
+      sessionNumber = 0;
     }
-    else {
-        //start short break
-        if (sessionNumber != NUM_OF_SESSIONS_IN_POMO) {
-            startTimer(SHORT_BREAK);
-        }
-        //start long break
-        else {
-            startTimer(LONG_BREAK);
-            sessionNumber = 0;
-        }
-    }
-};
+  }
+}
 
 function endTimerSession() {
-    clearInterval(timer);
+  clearInterval(timer);
 
-    //go to next pomo session if break and ended break early
-    if (isBreak) {
-        isBreak = false;
-    }
-    
-    updateUIForSessionEnd();
+  // go to next pomo session if break and ended break early
+  if (isBreak) {
+    isBreak = false;
+  }
 
-    startButton.removeEventListener("click", endTimerSession);
-    startButton.addEventListener("click", startTimerSession);
+  updateUIForSessionEnd();
+
+  startButton.removeEventListener('click', endTimerSession);
+  startButton.addEventListener('click', startTimerSession);
 }
 
 function updateUIForSessionEnd() {
-    startButton.value = "Start";
+  startButton.value = 'Start';
 
-    document.getElementById("pomo").style.textDecoration = "none";
-    document.getElementById("short-break").style.textDecoration = "none";
-    document.getElementById("long-break").style.textDecoration = "none";
-    
-    //is pomo session
-    if (!isBreak) {
-        document.getElementById("timer-box").style.background = "#9FEDD7";
-        document.getElementById("pomo").style.textDecoration = "underline";
-        setTimeOnUI(POMO_TIME, 0);
+  document.getElementById('pomo').style.textDecoration = 'none';
+  document.getElementById('short-break').style.textDecoration = 'none';
+  document.getElementById('long-break').style.textDecoration = 'none';
+
+  // is pomo session
+  if (!isBreak) {
+    document.getElementById('timer-box').style.background = '#9FEDD7';
+    document.getElementById('pomo').style.textDecoration = 'underline';
+    setTimeOnUI(POMO_TIME, 0);
+  } else {
+    const isShortBreak = sessionNumber !== NUM_OF_SESSIONS_IN_POMO;
+    if (isShortBreak) {
+      document.getElementById('timer-box').style.background = '#FEF9C7';
+      document.getElementById('short-break').style.textDecoration = 'underline';
+      setTimeOnUI(SHORT_BREAK, 0);
+    } else {
+      document.getElementById('timer-box').style.background = '#FCE181';
+      document.getElementById('long-break').style.textDecoration = 'underline';
+      setTimeOnUI(LONG_BREAK, 0);
     }
-    else {
-        //is short break
-        if (sessionNumber != NUM_OF_SESSIONS_IN_POMO) {
-            document.getElementById("timer-box").style.background = "#FEF9C7";
-            document.getElementById("short-break").style.textDecoration = "underline";
-            setTimeOnUI(SHORT_BREAK, 0);
-        }
-        //is long break
-        else {
-            document.getElementById("timer-box").style.background = "#FCE181";
-            document.getElementById("long-break").style.textDecoration = "underline";
-            setTimeOnUI(LONG_BREAK, 0);
-        }
-    }
+  }
 }
