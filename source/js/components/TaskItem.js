89fc9a907bbdc3bb4fc42c9aada4acb41369a672
Jose Falconi <42155184+jfalconi-cavallini@users.noreply.github.com>
2021-02-18 11:27:32 -0800

    Update TaskItem.js

--- source/js/components/TaskItem.js
+++ source/js/components/TaskItem.js
@@ -9,5 +9,19 @@ class TaskList extends HTMLElement {
     list.className = 'tasklist';
     return list;
   }
+  
+  createName(taskList) {
+    const taskName = taskList.appendChild(document.createElement('p'));
+    taskName.className = 'name';
+    taskName.textContent = this.getAttribute('name');
+    return taskName;
+  }
+  
+  createDuration(taskList) {
+    const taskDuration = taskList.appendChild(document.createElement('p'));
+    taskDuration.className = 'duration';
+    taskDuration.textContent = this.getAttribute('duration');
+    return taskDuration;
+  }
 
 }
