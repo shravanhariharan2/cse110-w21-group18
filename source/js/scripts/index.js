b519024313e34f988f37440dbfe5af79bdce0229
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-03-08 14:43:34 -0800

    Move settings/session event listeners to scripts

--- source/js/scripts/index.js
+++ source/js/scripts/index.js
@@ -2,7 +2,7 @@ import PomodoroSession from '../services/PomodoroSession.js';
 
 const pomodoroSession = new PomodoroSession();
 
-document.getElementById('start').addEventListener('click', () => {
-  Notification.requestPermission();
-  pomodoroSession.toggleSession;
+document.getElementById('start').addEventListener('click', async () => {
+  await Notification.requestPermission();
+  await pomodoroSession.toggleSession();
 });
