72f582eb23fdacb3eb36494fb25f52c73e4db71d
codefactor-io <support@codefactor.io>
2021-03-13 17:55:21 +0000

    [CodeFactor] Apply fixes

--- source/js/services/kbInteractions.js
+++ source/js/services/kbInteractions.js
@@ -9,7 +9,7 @@ class keyboardInteractions {
   constructor() {
     this.DEBUG = true;
     this.DOM_ELEMENTS = {
-      timerButton : document.getElementById('start'),
+      timerButton: document.getElementById('start'),
       taskList: document.getElementById('to-do-list'),
       addTaskButton: document.getElementById('add-task'),
       confirmTaskButton: document.getElementById('save-task'),
@@ -41,12 +41,11 @@ class keyboardInteractions {
   onKeyUp(event) {
     // mutex prevents certain operations from modifying the page when set to true
     this.kbMutex = false;
-    if(document.activeElement === this.DOM_ELEMENTS.inputTextField
-      || document.activeElement === this.DOM_ELEMENTS.notesTextField)
-      this.kbMutex = true;
+    if (document.activeElement === this.DOM_ELEMENTS.inputTextField
+      || document.activeElement === this.DOM_ELEMENTS.notesTextField) this.kbMutex = true;
       // check for space, tab, or enter
-      switch(event.keyCode) {
-        //space key
+      switch (event.keyCode) {
+        // space key
         case 32:
         this.handleSpace(event, this.kbMutex);
         break;
@@ -61,8 +60,7 @@ class keyboardInteractions {
 
         // non-cased keys
         default:
-        if(this.kbMutex)
-        return;
+        if (this.kbMutex) return;
         this.dprint(`key: ${event.keyCode} is not implemented`);
       }
     }
@@ -73,12 +71,10 @@ class keyboardInteractions {
     * @param {*} mutex a boolean used to restrict/negate function flow
     */
     handleSpace(event, mutex) {
-      if(mutex)
-      return;
-      else {
+      if (mutex) return;
+
         this.DOM_ELEMENTS.timerButton.click();
         this.dprint('Enter pressed');
-      }
     }
 
     /**
@@ -89,27 +85,23 @@ class keyboardInteractions {
 
     handleTab(event, mutex) {
       event.preventDefault();
-      if(mutex)
-      return;
+      if (mutex) return;
 
-      else {
         // prevent default tab function
         this.incrementIdx();
 
         // if the current focus is not a task then it must be the add task button
-        if(this.focusIdx === 0 ) {
+        if (this.focusIdx === 0) {
           this.DOM_ELEMENTS.inputBox.focus();
           // unclick the last task (so it's not highlighted)
           try {
             this.DOM_ELEMENTS.taskList.children[
               this.DOM_ELEMENTS.taskList.children.length - 1
             ].click();
-          }
-          catch(e) {
+          } catch (e) {
             this.dprint('No children to unclick');
           }
-        }
-        else {
+        } else {
           // collapse add task form if possible
           this.DOM_ELEMENTS.cancelInput.click();
 
@@ -117,7 +109,6 @@ class keyboardInteractions {
           this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].focus();
           document.activeElement.click();
         }
-      }
     }
 
     /**
@@ -126,33 +117,28 @@ class keyboardInteractions {
     * @param {*} mutex a boolean used to restrict/negate function flow
     */
     handleEnter(event, mutex) {
-      this.dprint(`focused idx: ${this.focusIdx}\n` +
-        `event key: ${event.keyCode}`
-      );
-      if(mutex)
-      return;
+      this.dprint(`focused idx: ${this.focusIdx}\n`
+        + `event key: ${event.keyCode}`);
+      if (mutex) return;
 
       event.stopImmediatePropagation();
       // handle enter on <add-task-button>
-      if(this.focusIdx === 0) {
+      if (this.focusIdx === 0) {
         this.dprint(`form display value: ${this.DOM_ELEMENTS.inputBox.style.display}`);
-        if(this.DOM_ELEMENTS.inputBox.style.display == 'none'
+        if (this.DOM_ELEMENTS.inputBox.style.display == 'none'
         || this.DOM_ELEMENTS.inputBox.style.display === undefined) {
           this.DOM_ELEMENTS.addTaskButton.click();
           this.DOM_ELEMENTS.inputTextField.focus();
-        }
-        else {
+        } else {
           // if task text box is non-empty then add task
-          if(this.DOM_ELEMENTS.inputTextField.value.length === 0)
-          this.DOM_ELEMENTS.cancelInput.click();
-          else
-          this.DOM_ELEMENTS.acceptInput.click();
+          if (this.DOM_ELEMENTS.inputTextField.value.length === 0) this.DOM_ELEMENTS.cancelInput.click();
+          else this.DOM_ELEMENTS.acceptInput.click();
         }
       }
       // handle enter on <task>
       else {
-        let shadow = this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].shadowRoot;
-        let childNodes = Array.from(shadow.childNodes);
+        const shadow = this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].shadowRoot;
+        const childNodes = Array.from(shadow.childNodes);
         // expand button is the 4th child
         childNodes[4].click();
       }
@@ -166,10 +152,8 @@ class keyboardInteractions {
     incrementIdx() {
       // TODO: update idx from mouse click (BLOCKAGE!!! No good way to track tasks)
       // increment if there are tasks
-      if(this.DOM_ELEMENTS.taskList.children.length > 0)
-      this.focusIdx = ((this.focusIdx+1)%(this.DOM_ELEMENTS.taskList.children.length + 1));
-      else
-      this.focusIdx = 0;
+      if (this.DOM_ELEMENTS.taskList.children.length > 0) this.focusIdx = ((this.focusIdx + 1) % (this.DOM_ELEMENTS.taskList.children.length + 1));
+      else this.focusIdx = 0;
     }
 
     /**
