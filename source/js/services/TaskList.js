bfcf3089dbca37f833a899822a55a88ce31f457d
Shane Benetz <shanebenetz@gmail.com>
2021-03-14 19:47:52 -0700

    info and hide/expand tasklist

--- source/js/services/TaskList.js
+++ source/js/services/TaskList.js
@@ -14,6 +14,7 @@ class TaskList {
     this.completedIsExpanded = false;
     this.hasLoadedIntoDOM = false; // make sure nothing else runs while loading
     this.hasActiveSession = false;
+    this.TLCollapsed = false;
     this.displayInputBox = this.displayInputBox.bind(this);
     this.addNotesToTask = this.addNotesToTask.bind(this);
     this.addTask = this.addTask.bind(this);
@@ -21,6 +22,8 @@ class TaskList {
     this.cancelInput = this.cancelInput.bind(this);
     this.loadTasks = this.loadTasks.bind(this);
     this.expandCompletedTasks = this.expandCompletedTasks.bind(this);
+    this.collapseTaskList = this.collapseTaskList.bind(this);
+    this.expandTaskList = this.expandTaskList.bind(this);
     this.DOM_ELEMENTS = {
       taskListTitle: document.getElementById('list-title'),
       addTaskButton: document.getElementById('add-task'),
@@ -36,6 +39,8 @@ class TaskList {
       completedListTitle: document.getElementById('completed-list-header'),
       expandCompleted: document.getElementById('expand-completed'),
       viewAll: document.getElementById('view-all'),
+      collapseTaskList: document.getElementById('collapse-task-list'),
+      expandTaskList: document.getElementById('expand-task-list'),
     };
     this.DOM_ELEMENTS.addTaskButton.addEventListener('click', this.displayInputBox);
     this.DOM_ELEMENTS.addNotesButton.addEventListener('click', this.addNotesToTask);
@@ -44,13 +49,32 @@ class TaskList {
     this.DOM_ELEMENTS.completedList.addEventListener('DOMSubtreeModified', this.listChanged);
     this.DOM_ELEMENTS.cancelButton.addEventListener('click', this.cancelInput);
     this.DOM_ELEMENTS.expandCompleted.addEventListener('click', this.expandCompletedTasks);
+    this.DOM_ELEMENTS.collapseTaskList.addEventListener('click', this.collapseTaskList);
+    this.DOM_ELEMENTS.expandTaskList.addEventListener('click', this.expandTaskList);
     this.DOM_ELEMENTS.viewAll.style.display = 'none';
     this.makeTasksDraggable();
     this.hideCompletedIfNoTasksExist();
     this.DOM_ELEMENTS.completedList.style.display = 'none';
     return instance;
   }
-
+  collapseTaskList(){
+    document.getElementById('right-half').style.animationDuration="1s";
+    document.getElementById('right-half').style.animationName = "slideout";
+    document.getElementById('right-half').style.marginLeft= "2000px";
+    document.getElementById('timer-box').style.animationDuration="1s";
+    document.getElementById('timer-box').style.animationName = "slideRight";
+    document.getElementById('timer-box').style.left = '68%';
+    setTimeout(() => {document.getElementById('expand-task-list').style.display = 'inline';},600);
+  }
+  expandTaskList(){
+    document.getElementById('right-half').style.animationDuration="1s";
+    document.getElementById('right-half').style.animationName = "slidein";
+    document.getElementById('right-half').style.marginLeft= "initial";
+    document.getElementById('timer-box').style.animationDuration="1s";
+    document.getElementById('timer-box').style.animationName = "slideLeft";
+    document.getElementById('timer-box').style.left = '45%';
+    document.getElementById('expand-task-list').style.display = 'none';;
+  }
   /**
    * Loads the tasks saved in sessionStorage back in the order that they were in previously
    */
@@ -166,9 +190,6 @@ class TaskList {
     this.DOM_ELEMENTS.completedList.style.display = 'none';
     this.DOM_ELEMENTS.viewAll.style.display = 'inline';
     const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (width > 1300) {
-      this.DOM_ELEMENTS.taskListTitle.style.marginTop = '30%';
-    }
   }
 
   /**
