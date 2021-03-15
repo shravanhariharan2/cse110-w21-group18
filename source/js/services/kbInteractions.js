c886f50f04029fb309b604075211727cb2c2a2f0
Austin <au.sophonsri@gmail.com>
2021-03-15 02:48:12 -0700

    More keyboard functions
    made new task form elements tabbable, also keybound expanding task list and completing tasks. Additionally implemented `shift + tab` to reverse item traversal direction

--- source/js/services/kbInteractions.js
+++ source/js/services/kbInteractions.js
@@ -16,49 +16,103 @@ class keyboardInteractions {
       inputBox: document.getElementById('task-add-input'),
       inputTextField: document.getElementById('add-task-name'),
       inputMultiField: document.getElementById('pomos'),
+      notesButton: document.getElementById('add-notes'),
       notesTextField: document.getElementById('add-task-description'),
       cancelInput: document.getElementById('cancel-input'),
       acceptInput: document.getElementById('save-task'),
+      expansionLabel: document.getElementById('view-all'),
     };
+
+    // All keys in this have their default keyup behaviour prevented
+    this.KEYCODES = {
+        spacebar: 32, 
+        tab: 9,
+        enter: 13,
+        equals: 187,
+        minus: 189,
+        right_arrow: 39,
+        // shift: 16,
+    };
+
+    this.MODKEY = 16;
+    this.modStatus = false;
 
     this.focusIdx = 0;
     this.kbMutex = false;
     this.DOM_ELEMENTS.inputBox.focus();
+    // if(this.DOM_ELEMENTS.taskList.children.length !== 0) {
+    //     // focus on fist task 
+    //     this.DOM_ELEMENTS.taskList.children[0].focus();
+    // }
 
     // bind functions
-    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
     this.dprint = this.dprint.bind(this);
     this.incrementIdx = this.incrementIdx.bind(this);
 
-    // add event listeners
-    document.addEventListener('keyup', this.onKeyUp, false);
+    // add key listener
+    document.addEventListener('keydown', this.onKeyDown, false);
+
+    // remove keyup actions on input elements
+    document.querySelector('input').addEventListener('keyup', function (e) {
+        // generate a blacklist from the values of keycodes
+        if(e.which !== this.MODKEY && Object.values(this.KEYCODES).includes(e.which)) {
+            e.preventDefault();
+        }
+    });
+
+    // add keyup listener
+    document.addEventListener('keyup', (e) => {if(e.keyCode === this.MODKEY) this.modUp();}, false);
   }
 
   /**
   * Checks key press ID and handles accordingly
   * @param {*} event the event object passed by an EventListener
   */
-  onKeyUp(event) {
+  onKeyDown(event) {
     // mutex prevents certain operations from modifying the page when set to true
-    this.kbMutex = false;
+    // disable space when typing in text boxes
+    let spaceMutex = false;
     if (document.activeElement === this.DOM_ELEMENTS.inputTextField
       || document.activeElement === this.DOM_ELEMENTS.notesTextField) {
-      this.kbMutex = true;
+      spaceMutex = true;
     }
+    // disable tab when working on tasks
+    let tabMutex = false;
+    if(this.DOM_ELEMENTS.expansionLabel.style.display !== 'none' && 
+        this.DOM_ELEMENTS.expansionLabel.style.display !== undefined) {
+            tabMutex = true;
+    }
+
     // check for space, tab, or enter
     switch (event.keyCode) {
       // space key
-      case 32:
-        this.handleSpace(event, this.kbMutex);
+      case this.KEYCODES.spacebar:
+        this.handleSpace(event, spaceMutex);
         break;
       // tab key
-      case 9:
-        this.handleTab(event, false);
+      case this.KEYCODES.tab:
+        this.handleTab(event, tabMutex);
         break;
       // enter key
-      case 13:
+      case this.KEYCODES.enter:
         this.handleEnter(event, false);
         break;
+    
+      // right arrow 
+      case this.KEYCODES.right_arrow:
+        this.handleRightArrow(event, false);
+        break;
+
+      // handle minus 
+      case this.KEYCODES.minus:
+          this.handleMinus(event, false);
+          break;
+
+      // handle MOD KEY
+      case this.MODKEY:
+          this.modDown();
+          break;
 
       // non-cased keys
       default:
@@ -87,30 +141,60 @@ class keyboardInteractions {
   */
 
   handleTab(event, mutex) {
+    // prevent default tab function
     event.preventDefault();
+    event.stopImmediatePropagation();
     if (mutex) {
       return;
     }
 
-    // prevent default tab function
-    this.incrementIdx();
+    // increment if not focused on text fields
+    if(this.DOM_ELEMENTS.inputBox.style.display === 'none') {
+            this.dprint("input box is none");
+            // if shift is not held down then increment, else decrement 
+            if(this.modStatus) {
+                this.decrementIdx();
+            } else {
+                this.incrementIdx();
+            }
+        }
 
     // if the current focus is not a task then it must be the add task button
     if (this.focusIdx === 0) {
-      this.DOM_ELEMENTS.inputBox.focus();
       // unclick the last task (so it's not highlighted)
       try {
-        this.DOM_ELEMENTS.taskList.children[
-          this.DOM_ELEMENTS.taskList.children.length - 1
-        ].click();
+        this.unclickItems();
       } catch (e) {
         this.dprint('No children to unclick');
       }
+        // cycle between inputs while form is active 
+        if(this.DOM_ELEMENTS.inputBox.style.display !== 'none' 
+        && this.DOM_ELEMENTS.inputBox.style.display !== undefined) {
+            if(document.activeElement === this.DOM_ELEMENTS.inputTextField) {
+                this.dprint("tab case 1");
+                this.DOM_ELEMENTS.inputMultiField.focus();
+            } else if(document.activeElement === this.DOM_ELEMENTS.inputMultiField) {
+                // if notes is focusable, then focus 
+                this.dprint("tab case 2");
+                if (this.DOM_ELEMENTS.notesTextField.style.display !== 'none') {
+                    this.DOM_ELEMENTS.notesTextField.focus();
+                } else {
+                    this.DOM_ELEMENTS.notesButton.focus();
+                }
+            }
+            else {
+                this.dprint("tab case default");
+                this.DOM_ELEMENTS.inputTextField.focus();
+            }
+        } else {
+            this.DOM_ELEMENTS.inputBox.focus();
+        }
     } else {
       // collapse add task form if possible
       this.DOM_ELEMENTS.cancelInput.click();
 
       // focus on the current task and highlight it
+      console.log(this.modStatus);
       this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].focus();
       document.activeElement.click();
     }
@@ -126,28 +210,88 @@ class keyboardInteractions {
       + `event key: ${event.keyCode}`);
     if (mutex) return;
 
-    event.stopImmediatePropagation();
+    event.preventDefault();
     // handle enter on <add-task-button>
     if (this.focusIdx === 0) {
       this.dprint(`form display value: ${this.DOM_ELEMENTS.inputBox.style.display}`);
+      // If the form is not open then open it
       if (this.DOM_ELEMENTS.inputBox.style.display === 'none'
       || this.DOM_ELEMENTS.inputBox.style.display === undefined) {
         this.DOM_ELEMENTS.addTaskButton.click();
         this.DOM_ELEMENTS.inputTextField.focus();
+      } else if (document.activeElement === this.DOM_ELEMENTS.notesButton) {
+        // the form is open and we are focused on adding notes, so open the notes text form
+        this.DOM_ELEMENTS.notesButton.click();
+        this.DOM_ELEMENTS.notesTextField.focus();
       } else if (this.DOM_ELEMENTS.inputTextField.value.length === 0) {
-        // if task text box is non-empty then add task
+        // form is open and we're not trying to add notes: if nothing is written, then cancel
         this.DOM_ELEMENTS.cancelInput.click();
       } else {
+        // add if content exists
         this.DOM_ELEMENTS.acceptInput.click();
       }
     } else {
       // handle enter on <task>
       const shadow = this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].shadowRoot;
       const childNodes = Array.from(shadow.childNodes);
-      // expand button is the 4th child
+      this.dprint(`shadowChildren: ${childNodes}`);
+      // expand button is the 5th child
       childNodes[4].click();
     }
   }
+
+  handleRightArrow(event, mutex) {
+      if(mutex) {
+          return;
+      }      
+      if(this.focusIdx !== 0) {
+          event.preventDefault();
+          const shadow = this.DOM_ELEMENTS.taskList.children[this.focusIdx - 1].shadowRoot;
+          const childNodes = Array.from(shadow.childNodes);
+          this.dprint(`shadowChildren: ${childNodes}`);
+          // checkbox is the 2nd child 
+          childNodes[1].click();
+      }
+  }
+
+  handleMinus(event, mutex) {
+    if(mutex) {
+        return;
+    }
+    // click the expansion label if shown 
+    if(this.DOM_ELEMENTS.expansionLabel.style.display !== 'none' && 
+        this.DOM_ELEMENTS.expansionLabel.style.display !== undefined) {
+            event.preventDefault();
+            this.DOM_ELEMENTS.expansionLabel.click();
+        }
+  }
+
+  modDown() {
+      this.modStatus = true;
+  }
+  modUp() {
+      this.modStatus = false;
+  }
+  
+
+   /**
+   * Unclick the elements (assumes that at least one task is clicked)
+   * No click tracking implemented so the unclicking function is bad
+   */
+  unclickItems() {
+    if(this.DOM_ELEMENTS.taskList.children.length > 1) {
+        this.DOM_ELEMENTS.taskList.children[
+            this.DOM_ELEMENTS.taskList.children.length - 2
+        ].click();
+        this.DOM_ELEMENTS.taskList.children[
+            this.DOM_ELEMENTS.taskList.children.length - 1
+        ].click();
+    }
+    this.DOM_ELEMENTS.taskList.children[
+      this.DOM_ELEMENTS.taskList.children.length - 1
+    ].click();
+}
+
 
   /**
   * updates the index (in case of mouse selection) then increments the index
@@ -163,6 +307,18 @@ class keyboardInteractions {
     } else {
       this.focusIdx = 0;
     }
+  }
+
+  decrementIdx() {
+    // javascript does not implement circular modulo
+    if (this.DOM_ELEMENTS.taskList.children.length > 0) {
+        this.focusIdx = ((this.focusIdx - 1)
+            % (this.DOM_ELEMENTS.taskList.children.length + 1)
+            + (this.DOM_ELEMENTS.taskList.children.length + 1)) 
+        %(this.DOM_ELEMENTS.taskList.children.length + 1);
+      } else {
+        this.focusIdx = 0;
+      }
   }
 
   /**
